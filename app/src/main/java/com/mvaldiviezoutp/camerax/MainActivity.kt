b27162882623dcd8ce42371f0e209691155156e2
michael_abx <mavaldiviezo.abx@gmail.com>
2023-11-03 11:58:05 -0500

    camera

--- app/src/main/java/com/mvaldiviezoutp/camerax/MainActivity.kt
+++ app/src/main/java/com/mvaldiviezoutp/camerax/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.mvaldiviezoutp.camerax
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.graphics.ImageFormat
 import android.graphics.SurfaceTexture
 import android.hardware.camera2.CameraCaptureSession
 import android.hardware.camera2.CameraDevice
 import android.hardware.camera2.CameraManager
 import android.hardware.camera2.CaptureRequest
+import android.media.ImageReader
 import android.os.Bundle
+import android.os.Environment
 import android.os.Handler
 import android.os.HandlerThread
 import android.view.Surface
 import android.view.TextureView
-import android.widget.TextView
+import android.widget.Button
+import android.widget.Toast
+import androidx.annotation.NonNull
+import androidx.annotation.RequiresPermission
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.File
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,6 +37,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var textureView: TextureView
     private lateinit var cameraCaptureSession: CameraCaptureSession
     private lateinit var cameraDevice: CameraDevice
+    private lateinit var imageReader: ImageReader
+    private var imageCounter = 0
+
+    private val CAMERA_PERMISSION_CODE = 101
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +58,13 @@ class MainActivity : AppCompatActivity() {
                 width: Int,
                 height: Int
             ) {
+                if (ActivityCompat.checkSelfPermission(
+                        this@MainActivity,
+                        Manifest.permission.CAMERA
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    return
+                }
                 openCamera()
             }
 
@@ -61,52 +84,79 @@ class MainActivity : AppCompatActivity() {
 
             }
         }
+
+        imageReader = ImageReader.newInstance(1080, 1920, ImageFormat.JPEG, 1)
+        imageReader.setOnImageAvailableListener({ reader ->
+            val image = reader.acquireLatestImage()
+            val buffer = image!!.planes[0].buffer
+            val bytes = ByteArray(buffer.remaining())
+            buffer.get(bytes)
+
+            val imageFileName = createImageFileName()
+            val file = File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), imageFileName)
+            val opStream = FileOutputStream(file)
+            opStream.write(bytes)
+            opStream.close()
+            image.close()
+            Toast.makeText(this@MainActivity, "Image captured: $imageFileName", Toast.LENGTH_SHORT).show()
+        }, handler)
+
+        findViewById<Button>(R.id.capture).apply {
+            setOnClickListener {
+                capReq = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE)
+                capReq.addTarget(imageReader.surface)
+                cameraCaptureSession.capture(capReq.build(), null, null)
+            }
+        }
     }
 
-    @SuppressLint("MissingPermission")
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraCaptureSession.close()
+        cameraDevice.close()
+        handler.removeCallbacksAndMessages(null)
+        handlerThread.quitSafely()
+    }
+
+    @RequiresPermission(Manifest.permission.CAMERA)
     private fun openCamera() {
-        cameraManager.openCamera(cameraManager.cameraIdList[0], object : CameraDevice.StateCallback() {
+        val cameraId = cameraManager.cameraIdList[0]
+        cameraManager.openCamera(cameraId, object : CameraDevice.StateCallback() {
             override fun onOpened(device: CameraDevice) {
                 cameraDevice = device
 
-                capReq = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                val capReq = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
                 val surface = Surface(textureView.surfaceTexture)
                 capReq.addTarget(surface)
 
-                cameraDevice.createCaptureSession(listOf(surface), object : CameraCaptureSession.StateCallback() {
-                    override fun onConfigureFailed(session: CameraCaptureSession) {
-
-                    }
-
+                cameraDevice.createCaptureSession(listOf(surface, imageReader.surface), object : CameraCaptureSession.StateCallback() {
                     override fun onConfigured(session: CameraCaptureSession) {
                         cameraCaptureSession = session
                         cameraCaptureSession.setRepeatingRequest(capReq.build(), null, null)
+                    }
+
+                    override fun onConfigureFailed(session: CameraCaptureSession) {
+                        // Tratar la falla de configuración aquí si es necesario
                     }
                 }, handler)
             }
 
             override fun onDisconnected(device: CameraDevice) {
-
+                // Manejar la desconexión de la cámara aquí si es necesario
             }
 
             override fun onError(device: CameraDevice, error: Int) {
+                // Manejar errores de la cámara aquí, por ejemplo, mostrar un mensaje de error al usuario
                 cameraDevice.close()
             }
         }, handler)
     }
 
     private fun getPermissions() {
-        val permissions = mutableListOf<String>()
-
-        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
-            permissions.add(Manifest.permission.CAMERA)
-        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
-            permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE)
-        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
-            permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-
-        if (permissions.isNotEmpty()) {
-            requestPermissions(permissions.toTypedArray(), 101)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
+        } else {
+            // Permisos ya concedidos
         }
     }
 
@@ -116,10 +166,27 @@ class MainActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        grantResults.forEach {
-            if (it == PackageManager.PERMISSION_GRANTED) {
-                // Los permisos fueron concedidos, puedes realizar acciones aquí
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permiso de cámara concedido, puedes abrir la cámara
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.CAMERA
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    return
+                }
+                openCamera()
+            } else {
+                Toast.makeText(this, "Se requieren permisos de cámara para usar esta aplicación", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    private fun createImageFileName(): String {
+        val timestamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val imageFileName = "IMG_${timestamp}_$imageCounter.jpg"
+        imageCounter++
+        return imageFileName
     }
 }
